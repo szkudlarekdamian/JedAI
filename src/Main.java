7acb5483d2a5f42421572d13218e3e08a2caa393
wojtas1411 <wojtas.t.1997@gmail.com>
2020-06-08 22:46:31 +0200

    dummy grid search

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
+import org.apache.jena.base.Sys;
 import org.scify.jedai.blockbuilding.AbstractBlockBuilding;
 import org.scify.jedai.blockbuilding.ExtendedSortedNeighborhoodBlocking;
 import org.scify.jedai.blockprocessing.AbstractBlockProcessing;
@@ -19,8 +22,12 @@ import org.scify.jedai.utilities.datastructures.BilateralDuplicatePropagation;
 import org.scify.jedai.utilities.enumerations.RepresentationModel;
 import org.scify.jedai.utilities.enumerations.SimilarityMetric;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.time.Duration;
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 
@@ -30,6 +37,8 @@ public class Main {
     private final static String GOOGLE_PROFILE_FILEPATH = "data/gpProfiles";
     private final static String GROUND_TRUTH_FILEPATH = "data/amazonGpIdDuplicates";
 
+    private static boolean verbose = true;
+
     private static List<AbstractBlock> blockBuilding(AbstractBlockBuilding blockBuilding,
                                                      List<EntityProfile> profiles1,
                                                      List<EntityProfile> profiles2,
@@ -38,9 +47,12 @@ public class Main {
         List<AbstractBlock> blocks = blockBuilding.getBlocks(profiles1, profiles2);
         Instant end = Instant.now();
 
-        BlocksPerformance blockBuildingStats = new BlocksPerformance(blocks, duplicatePropagation);
-        blockBuildingStats.setStatistics();
-        blockBuildingStats.printStatistics(Duration.between(start,end).toNanos(),blockBuilding.getMethodConfiguration(), blockBuilding.getMethodName());
+        if (verbose) {
+            BlocksPerformance blockBuildingStats = new BlocksPerformance(blocks, duplicatePropagation);
+            blockBuildingStats.setStatistics();
+            blockBuildingStats.printStatistics(Duration.between(start,end).toMillis(),blockBuilding.getMethodConfiguration(), blockBuilding.getMethodName());
+        }
+
         return blocks;
     }
 
@@ -51,9 +63,12 @@ public class Main {
         List<AbstractBlock> newBlocks = blockProcessing.refineBlocks(blocks);
         Instant end = Instant.now();
 
-        BlocksPerformance blockProcessingStats = new BlocksPerformance(newBlocks, duplicatePropagation);
-        blockProcessingStats.setStatistics();
-        blockProcessingStats.printStatistics(Duration.between(start,end).toNanos(), blockProcessing.getMethodConfiguration(), blockProcessing.getMethodName());
+        if (verbose) {
+            BlocksPerformance blockProcessingStats = new BlocksPerformance(newBlocks, duplicatePropagation);
+            blockProcessingStats.setStatistics();
+            blockProcessingStats.printStatistics(Duration.between(start,end).toMillis(), blockProcessing.getMethodConfiguration(), blockProcessing.getMethodName());
+        }
+
         return newBlocks;
     }
 
@@ -63,11 +78,14 @@ public class Main {
         SimilarityPairs similarityPairs = entityMatching.executeComparisons(blocks);
         Instant end = Instant.now();
 
-        System.out.println(entityMatching.getMethodName());
-        System.out.println(entityMatching.getMethodInfo());
-        System.out.println(entityMatching.getMethodConfiguration());
-        System.out.println(entityMatching.getMethodParameters());
-        System.out.println("Elapsed time: " + Duration.between(start, end).toMillis());
+        if (verbose) {
+            System.out.println(entityMatching.getMethodName());
+            System.out.println(entityMatching.getMethodInfo());
+            System.out.println(entityMatching.getMethodConfiguration());
+            System.out.println(entityMatching.getMethodParameters());
+            System.out.println("Elapsed time: " + Duration.between(start, end).toMillis());
+        }
+
         return similarityPairs;
     }
 
@@ -80,8 +98,38 @@ public class Main {
 
         ClustersPerformance clustersPerformance = new ClustersPerformance(clusters, duplicatePropagation);
         clustersPerformance.setStatistics();
-        clustersPerformance.printStatistics(Duration.between(start, end).toMillis(), clusteringMethod.getMethodConfiguration(), clusteringMethod.getMethodName());
+        if (verbose) {
+            clustersPerformance.printStatistics(Duration.between(start, end).toMillis(), clusteringMethod.getMethodConfiguration(), clusteringMethod.getMethodName());
+        }
+
         return clustersPerformance.getFMeasure();
+    }
+
+    public static double run(List<EntityProfile> profiles1,
+                             List<EntityProfile> profiles2,
+                             AbstractDuplicatePropagation duplicatePropagation,
+                             int windowSize,
+                             double blockFilterThreshold,
+                             RepresentationModel representationModel,
+                             SimilarityMetric similarityMetric,
+                             double entityMatchingSimilarityThreshold,
+                             double entityClusteringSimilarityThreshold) {
+        List<AbstractBlock> blocks;
+
+        AbstractBlockBuilding blockBuilding = new ExtendedSortedNeighborhoodBlocking(windowSize);
+        blocks = blockBuilding(blockBuilding, profiles1, profiles2, duplicatePropagation);
+
+        AbstractBlockProcessing blockFiltering = new BlockFiltering(blockFilterThreshold);
+        blocks = blockProcessing(blockFiltering, blocks, duplicatePropagation);
+
+        AbstractComparisonCleaning comparisonPropagation = new ComparisonPropagation();
+        blocks = blockProcessing(comparisonPropagation, blocks, duplicatePropagation);
+
+        AbstractEntityMatching entityMatchingStrategy = new GroupLinkage(entityMatchingSimilarityThreshold, profiles1, profiles2, representationModel, similarityMetric);
+        SimilarityPairs similarityPairs = entityMatching(entityMatchingStrategy, blocks);
+
+        AbstractEntityClustering clusteringMethod = new RicochetSRClustering(entityClusteringSimilarityThreshold);
+        return entityClustering(clusteringMethod, similarityPairs, duplicatePropagation);
     }
 
     public static void main(String[] args) {
@@ -94,58 +142,117 @@ public class Main {
 
         Set<IdDuplicates> duplicates = truth.getDuplicatePairs(amazonProfiles, googleProfiles);
 
-        BilateralDuplicatePropagation duplicatePropagation = new BilateralDuplicatePropagation(duplicates);
-        List<AbstractBlock> blocks;
+        AbstractDuplicatePropagation duplicatePropagation = new BilateralDuplicatePropagation(duplicates);
 
-        /*
-         * Extended Sorted Neighbourhood
-         * window size param (constructor) [2, ...]
-         */
-        System.out.println("F1: Extended Sorted Neighbourhood");
-        AbstractBlockBuilding blockBuilding = new ExtendedSortedNeighborhoodBlocking(2);
+        verbose = false;
 
-        blocks = blockBuilding(blockBuilding, amazonProfiles, googleProfiles, duplicatePropagation);
+//        double result = run(amazonProfiles, googleProfiles, duplicatePropagation,
+//                2, 0.5,
+//                RepresentationModel.TOKEN_UNIGRAMS_TF_IDF,
+//                SimilarityMetric.COSINE_SIMILARITY,
+//                0.1, 0.1);
 
-        /*
-         * Block Filtering
-         * r (threshold??) parameter (constructor) [0, 1]
-         */
-        System.out.println("\n\nF2: Block Filtering");
-        AbstractBlockProcessing blockFiltering = new BlockFiltering(0.8);
+        int[] windowSizes = {2,3,5,7,9};//,11,13,15,17,19};
+        double[] thresholds = {0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9};
+        RepresentationModel[] representationModels = {RepresentationModel.TOKEN_UNIGRAMS_TF_IDF};//, RepresentationModel.TOKEN_UNIGRAMS};
+        SimilarityMetric[] similarityMetrics = {SimilarityMetric.COSINE_SIMILARITY};//, SimilarityMetric.JACCARD_SIMILARITY};
 
-        blocks = blockProcessing(blockFiltering, blocks, duplicatePropagation);
+        List<String[]> recordsList = new ArrayList<>();
 
-        /*
-         * Comparison propagation
-         * no parameters taken, nothing to optimize here
-         */
-        System.out.println("\n\nF3: Comparison propagation");
-        AbstractComparisonCleaning comparisonPropagation = new ComparisonPropagation();
+        // grid search
+        for (int windowSize: windowSizes) {
+            for(double bfT: thresholds) {
+                for(RepresentationModel rm: representationModels) {
+                    for(SimilarityMetric sm: similarityMetrics) {
+                        for(double emT: thresholds) {
+                            for(double ecT: thresholds) {
+                                Instant start = Instant.now();
+                                double result = run(amazonProfiles, googleProfiles, duplicatePropagation,
+                                        windowSize, bfT, rm, sm, emT, ecT);
+                                Instant end = Instant.now();
+                                long elapsed = Duration.between(start, end).toMillis();
+                                String[] record = new String[]{
+                                        String.valueOf(elapsed),
+                                        String.valueOf(windowSize),
+                                        String.valueOf(bfT),
+                                        rm.name(),
+                                        sm.name(),
+                                        String.valueOf(emT),
+                                        String.valueOf(ecT),
+                                        String.valueOf(result)
+                                };
+                                recordsList.add(record);
+                                System.out.println(Arrays.toString(record));
+                            }
+                        }
+                    }
+                }
+            }
+        }
 
-        blocks = blockProcessing(comparisonPropagation, blocks, duplicatePropagation);
+        try {
+            FileWriter out = new FileWriter(Instant.now().toString() + "_results.csv");
+            CSVPrinter printer = new CSVPrinter(out, CSVFormat.DEFAULT);
+            printer.printRecords(recordsList);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-        /*
-         * Group linkage
-         * Choose representation, similarity metric, threshold
-         */
-        System.out.println("\n\nF4: Group Linkage");
-        RepresentationModel representationModel = RepresentationModel.TOKEN_UNIGRAMS_TF_IDF;
-        SimilarityMetric similarityMetric = SimilarityMetric.getModelDefaultSimMetric(representationModel);
-        AbstractEntityMatching entityMatchingStrategy = new GroupLinkage(0.1, amazonProfiles, googleProfiles, representationModel, similarityMetric);
 
-        SimilarityPairs similarityPairs = entityMatching(entityMatchingStrategy, blocks);
+//        List<AbstractBlock> blocks;
+//
+//        /*
+//         * Extended Sorted Neighbourhood
+//         * window size param (constructor) [2, ...]
+//         */
+//        System.out.println("F1: Extended Sorted Neighbourhood");
+//        AbstractBlockBuilding blockBuilding = new ExtendedSortedNeighborhoodBlocking(2);
+//        System.out.println(blockBuilding.getMethodParameters());
+//
+//        blocks = blockBuilding(blockBuilding, amazonProfiles, googleProfiles, duplicatePropagation);
+//
+//        /*
+//         * Block Filtering
+//         * r (threshold??) parameter (constructor) [0, 1]
+//         */
+//        System.out.println("\n\nF2: Block Filtering");
+//        AbstractBlockProcessing blockFiltering = new BlockFiltering(0.5);
+//        System.out.println(blockFiltering.getMethodParameters());
+//
+//        blocks = blockProcessing(blockFiltering, blocks, duplicatePropagation);
+//
+//        /*
+//         * Comparison propagation
+//         * no parameters taken, nothing to optimize here
+//         */
+//        System.out.println("\n\nF3: Comparison propagation");
+//        AbstractComparisonCleaning comparisonPropagation = new ComparisonPropagation();
+//
+//        blocks = blockProcessing(comparisonPropagation, blocks, duplicatePropagation);
+//
+//        /*
+//         * Group linkage
+//         * Choose representation, similarity metric, threshold
+//         */
+//        System.out.println("\n\nF4: Group Linkage");
+//        RepresentationModel representationModel = RepresentationModel.TOKEN_UNIGRAMS_TF_IDF;
+//        SimilarityMetric similarityMetric = SimilarityMetric.getModelDefaultSimMetric(representationModel);
+//        AbstractEntityMatching entityMatchingStrategy = new GroupLinkage(0.1, amazonProfiles, googleProfiles, representationModel, similarityMetric);
+//
+//        SimilarityPairs similarityPairs = entityMatching(entityMatchingStrategy, blocks);
+//
+//        /*
+//         * Ricochet SR Clustering
+//         */
+//        System.out.println("\n\nF5: Ricochet SR Clustering");
+//        AbstractEntityClustering clusteringMethod = new RicochetSRClustering(0.1);
+//        System.out.println(clusteringMethod.getMethodParameters());
+//
+//        double result = entityClustering(clusteringMethod, similarityPairs, duplicatePropagation);
 
-        /*
-         * Ricochet SR Clustering
-         */
-        System.out.println("\n\nF5: Ricochet SR Clustering");
-        AbstractEntityClustering clusteringMethod = new RicochetSRClustering(0.1);
-
-        double result = entityClustering(clusteringMethod, similarityPairs, duplicatePropagation);
-
-        System.out.println("\n\n");
-        System.out.println("-----------------");
-        System.out.println("Result [F1-score]: " + result);
-        System.out.println("-----------------");
+//        System.out.println("\n\n");
+//        System.out.println("-----------------");
+//        System.out.println("Result [F1-score]: " + result);
+//        System.out.println("-----------------");
     }
 }
